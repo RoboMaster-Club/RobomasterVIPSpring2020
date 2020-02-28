0f19ba67516ae1ff431188b31323df8d9c16c4b7
Lazloian <henry.silva.999@gmail.com>
2020-02-28 14:19:26 -0500

    updated driver tests

--- Electrical/driverTest/driverTest.ino
+++ Electrical/driverTest/driverTest.ino
@@ -4,7 +4,7 @@ int phaseA = 8;
 int phaseB = 7;
 int modePin = 4;
 
-int testMode = 1; // what test to run, 1: duty cycle from 100% to 0%, 2: change phase at 100% duty cycle
+int testMode = 2; // what test to run, 1: duty cycle from 100% to 0%, 2: change phase at 100% duty cycle
 
 void setup()
 {
@@ -36,14 +36,74 @@ void loop()
 
     else if (testMode == 2)
     {
-        digitalWrite(enableA, 0);
-        digitalWrite(enableB, 0);
+        digitalWrite(phaseA, 0);
+        digitalWrite(phaseB, 0);
 
-        delay(1000);
+        delay(100);
 
-        digitalWrite(enableA, 1);
-        digitalWrite(enableB, 1);
+        digitalWrite(phaseA, 1);
+        digitalWrite(phaseB, 1);
 
-        delay(1000);
+        delay(100);
     }
+
+    else if (testMode == 3)
+    {
+      digitalWrite(phaseA, 0);
+      digitalWrite(phaseB, 0);
+      for (int i = 255; i >= 0; i--)
+      {
+        analogWrite(enableA, i);
+        analogWrite(enableB, i);
+        delay(10); 
+      }
+      
+      digitalWrite(phaseA, 1);
+      digitalWrite(phaseB, 1);
+      for (int i = 0; i <= 255; i++)
+      {
+        analogWrite(enableA, i);
+        analogWrite(enableB, i);
+        delay(10);
+      }
+    }
+
+    else if (testMode == 4)
+    {
+      digitalWrite(phaseA, 1);
+      digitalWrite(phaseB, 1);
+      analogWrite(enableA, 255);
+      analogWrite(enableB, 255);
+
+      delay(1000);
+
+      digitalWrite(phaseA, 0);
+      digitalWrite(phaseB, 0);
+      analogWrite(enableA, 255);
+      analogWrite(enableB, 255);
+
+      delay(1000);
+
+      digitalWrite(phaseA, 1);
+      digitalWrite(phaseB, 1);
+      analogWrite(enableA, 127);
+      analogWrite(enableB, 127);
+
+      delay(1000);
+
+      digitalWrite(phaseA, 0);
+      digitalWrite(phaseB, 0);
+      analogWrite(enableA, 127);
+      analogWrite(enableB, 127);
+
+      delay(1000);
+
+      digitalWrite(phaseA, 1);
+      digitalWrite(phaseB, 1);
+      analogWrite(enableA, 0);
+      analogWrite(enableB, 0);
+
+      delay(1000);
+    }
+    
 }
